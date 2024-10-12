0decbeb35c107962f6d36f998a0033a4c8f50d22
Eyiram Gaze <eyiramgaze@gmail.com>
2024-10-12 11:49:38 +0000

    Modified Country class

---
+++ src/part1/Country.java
@@ -0,0 +1,22 @@
+package part1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Country {
+    private String name;
+    private Map<Integer, Emission> yearEmissions;
+
+    public Country(String name, Map<Integer, Emission> yearEmissions){
+        this.name = name;
+        this.yearEmissions = yearEmissions;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Map<Integer, Emission> getEmissions() {
+        return yearEmissions;
+    }
+}
